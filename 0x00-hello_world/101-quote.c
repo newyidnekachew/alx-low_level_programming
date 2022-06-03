6ea4e359666a0d3c6b67c83287c065c7bb492741
newyidnekachew <nhattyaman@gmail.com>
2022-06-02 23:33:11 -0700

    for commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
+#include<stdio.h
+#include<unistd.h>
 /**
  *main-error function
  *
  *Return:1
  */
-#include<stdio.h>
-
 int main(void)
 {
   write(1,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
