7f8b25922cf2305242269349e1b39af90a9e0b3d
newyidnekachew <nhattyaman@gmail.com>
2022-06-03 02:05:44 -0700

    life commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main-print out the last part of a quote in the standard error.
+ *main-Entry point
  *
- *Return: 1 if success.
+ *Return: 1 
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+char ch[] =  "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
