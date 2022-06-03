d8fb2325552890df162eeaa6f79f8911fb07e996
newyidnekachew <nhattyaman@gmail.com>
2022-06-02 18:57:05 -0700

    bety commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include<unistd.h>
-/**                                                                             *main-Entry point                                                              *                                                                              *Return:Always 1 (success)                                                     */
+/**                                                                             *main-error function                                                           *                                                                              *Return:Always 1 (success)                                                     */
 int main(void)
 {
 write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
