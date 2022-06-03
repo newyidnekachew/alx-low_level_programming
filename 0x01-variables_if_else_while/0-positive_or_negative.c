97694be22bd5e423f04e33f40fc37e07a9dcc6dd
newyidnekachew <nhattyaman@gmail.com>
2022-06-03 14:20:48 -0700

    if commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *
+ *Return:Always 0 (success)
+int main()
+{
+
+int n;
+
+if ( n >0 ){
+printf ("the number is positive: %d\n", n);
+}
+else if (n == 0) {
+printf (the number is zero: %d\n", n);
+}
+ealse {
+printf (the number is negative: %d\n", n);
+}
+return (0);
+
+}
