f5b924caf35e42941e92533bf1fa6d836aa52347
newyidnekachew <nhattyaman@gmail.com>
2022-06-02 18:35:23 -0700

    yidena commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,6 @@
 /**                                                                             *main-Entry point                                                              *                                                                              *Return:Always 1 (success)                                                     */
 int main(void)
 {
-char str1[]="and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
-write(2,str1,59);
+write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
