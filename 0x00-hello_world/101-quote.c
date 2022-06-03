28feaba82e89f7b214cc742854c22b3c31a1f06d
newyidnekachew <nhattyaman@gmail.com>
2022-06-03 02:21:43 -0700

    hello commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
  *main-Entry point
  *
- *Return: 1 
+ *Return: Always 0 (success) 
  */
 int main(void)
 {
-char ch[] =  "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
+write(1, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
