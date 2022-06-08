2f8ed4f5e0823608817ba3f8b38d1f3f1c6cfe20
newyidnekachew <nhattyaman@gmail.com>
2022-06-08 05:13:58 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+int main "main.h"
+/**
+*main - program that prints _putchar
+*
+*Description: prints _putchar
+*Return: always 0 (Success)
+*/
+int main(void)
+{
+char i[] = "_putchar\n";
+int j;
+for (j = 0; j <= 8; j++)
+{
+_putchar(i[j]);
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - program that print alphabet
+ *print_alphabet - program that print alphabet
+ *Description: prints lowwer case alphabet
+ *Return: Always 0.
+ *
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
