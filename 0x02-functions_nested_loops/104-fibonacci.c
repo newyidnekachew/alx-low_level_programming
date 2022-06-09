336a57bb035e3bc2bcc72726dd61c7636e232d02
newyidnekachew <nhattyaman@gmail.com>
2022-06-09 08:23:51 -0700

    this commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,11 +18,11 @@ i = j;
 j = k;
 printf("%lu, ", k);
 }
- m = i % 1000;
+m = i % 1000;
 i = i / 1000;
 n = j % 1000;
 j = j / 1000;
- while (count <= 98)
+while (count <= 98)
 {
 carry = (m + n) / 1000;
 p = (m + n) - carry * 1000;
