187b4ce8b57c54a0b537e71832b7786b80b75a4a
newyidnekachew <nhattyaman@gmail.com>
2022-06-09 08:07:03 -0700

    finale commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - calculate the absolute value of the number from zero.
+ *@n: the int to check
+ *Return: the absolute value of int
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
