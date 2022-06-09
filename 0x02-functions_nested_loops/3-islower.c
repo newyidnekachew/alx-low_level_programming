4969f85fd82e8af4d503495a23404c6a4be9329d
newyidnekachew <nhattyaman@gmail.com>
2022-06-09 04:54:43 -0700

    taske commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_islower - checks for lowercase character
- *@c: character to test.
+ *@c: the character to check.
  *Return: 1 if c is lowercase letter, 0 otherwise
  */
 int _islower(int c)
