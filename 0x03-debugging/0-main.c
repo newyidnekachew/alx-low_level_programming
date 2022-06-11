6def4f78941d6d4d423f99ae02faaf2cbd16c0c8
newyidnekachew <nhattyaman@gmail.com>
2022-06-11 04:00:47 -0700

    my commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,10 +3,14 @@
  *main - Test function for positive or negetive
  *Return: 0
  */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negetive(i);
-return (0);
+       int i;
+     
+       i = 0;
+       positive_or_negetive(i);
+     
+       return (0);
 }
+ 
