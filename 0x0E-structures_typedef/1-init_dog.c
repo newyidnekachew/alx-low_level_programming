339372cf6201fe7e12f366dff8c38e22775d9040
newyidnekachew <nhattyaman@gmail.com>
2022-07-04 11:49:30 -0700

    my commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,11 +8,11 @@
  * @owner:pointer to a string
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{ 
-if (d != NULL)   
-{      
-d->name = name;      
-d->age = age;      
-d->owner = owner;      
-}  
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
 }
