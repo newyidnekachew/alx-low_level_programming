8cc5138d57fda17a5f77807e12136e702524658a
newyidnekachew <nhattyaman@gmail.com>
2022-07-04 12:05:05 -0700

    my commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
-#endi
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+#endif
