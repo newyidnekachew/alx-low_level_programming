e15ada3010a90c8082e52eac38ad8f04294e45e0
newyidnekachew <nhattyaman@gmail.com>
2022-07-18 13:11:57 -0700

    my commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,7 +8,7 @@ size_t free_listint_safe(listint_t **h);
  * Return: If the list is not looped - 0.
  * Otherwise - the number of unique nodes in the list.
  */
-size_t looped_listint_count(listint_t *head)
+size_t free_listint_safe(listint_t **head)
 {
 listint_t *tortoise, *hare;
 size_t nodes = 1;
